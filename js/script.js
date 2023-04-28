8ce4168e25d337fd63021159851264c4ff6952c6
jhonny sousa <72554112+blackzinho321@users.noreply.github.com>
2023-04-28 00:37:01 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+let pessoaMaisvelha = prompt('Qua a nome da pessoa mais velha')
+let idadePessoamaisvelha = prompt('Qual idade da pessoa mais velha')
+
+let nomePessoaMaisnova = prompt('Qual Nome da pessoa mais nova')
+let idadedaPessoamaisnova = prompt('Qua a Idade da pessoa mais noma')
+
+let diferencadeidade = idadePessoamaisvelha - idadedaPessoamaisnova
+
+window.alert('')
