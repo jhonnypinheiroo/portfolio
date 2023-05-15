c311a1036caeae777ba9d018b14d3928556c1ef8
jhonny sousa <72554112+blackzinho321@users.noreply.github.com>
2023-05-15 20:45:33 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-
-let pessoaMaisvelha = prompt('Qua a nome da pessoa mais velha')
-let idadePessoamaisvelha = prompt('Qual idade da pessoa mais velha')
-
-let nomePessoaMaisnova = prompt('Qual Nome da pessoa mais nova')
-let idadedaPessoamaisnova = prompt('Qua a Idade da pessoa mais nova')
-
-let diferencadeidade = idadePessoamaisvelha - idadedaPessoamaisnova
-
-alert("O nome da pessao mais velha: " + pessoaMaisvelha + "\n\nNome da pessoa mais nova: " + nomePessoaMaisnova + "\n\nA diferença de idade entre as duas: " + diferencadeidade + " Anos" )
+console.log("aula sobre function")
